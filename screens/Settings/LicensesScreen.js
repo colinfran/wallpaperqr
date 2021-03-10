71083cd268bbf74d5814bd88e13acb900b629b9d
Colin Franceschini <colin.franceschini@gmail.com>
2021-03-10 15:17:35 -0800

    update

--- screens/Settings/LicensesScreen.js
+++ screens/Settings/LicensesScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { WebView } from 'react-native-webview';
 
-export default function FAQScreen() {
+export default function LicensesScreen() {
   return (
     <WebView
+    scalesPageToFit={false}
       source={{
-        uri: 'https://github.com/facebook/react-native',
+        uri: 'https://colinfran.github.io/wallpaperqr/license.html',
       }}
       style={{ marginTop: 20 }}
     />
